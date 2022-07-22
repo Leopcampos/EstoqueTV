1ece7af0fb6bab0747dec61d57698dc1737f4612
Leonardo Campos <57239577+Leopcampos@users.noreply.github.com>
2022-07-22 15:45:03 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,26 +1,3 @@
-﻿/* criar a classe Produto
- * 
- * -Nome : string
- * -Preco : double
- * -Quantidade: int
-
-Métodos
-
-ValorTotalEmEstoque() : double
-AdicionarProdutos(quantidade : int) : void
-RemoverProdutos(quantidade : int) : void
-
-Entre os dados do produto:
-Nome: TV
-Preço: 900.00
-Quantidade no estoque: 10
-Dados do produto: TV, $ 900.00, 10 unidades, Total: $ 9000.00
-Digite o número de produtos a ser adicionado ao estoque: 5
-Dados atualizados: TV, $ 900.00, 15 unidades, Total: $ 13500.00
-Digite o número de produtos a ser removido do estoque: 3
-Dados atualizados: TV, $ 900.00, 12 unidades, Total: $ 10800.00
-
-*/
 using System;
 using System.Globalization;
 
